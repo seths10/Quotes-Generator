6be262b2a531a2f0eb0c325ec72c98b20454cff6
Sreeharan <sree23haran@gmail.com>
2022-10-08 15:44:06 +0530

    Added few more quotes

--- js/quotes.js
+++ js/quotes.js
@@ -1283,6 +1283,30 @@ const quotes = [
     text: "Don't take rest after your first victory because if you fail in second, more lips are waiting to say that your first victory was just luck.",
     author: "Dr. A.P.J Abdul Kalam",
   },
+  {
+    text: "Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
+  {
+    text: "To succeed in your mission, you must have single-minded devotion to your goal.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
+  {
+    text: "If you fail, never give up because FAIL means First Attempt In Learning.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
+  {
+    text: "Creativity is seeing the same thing but thinking differently.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
+  {
+    text: "Failure will never overtake me if my determination to succeed is strong enough.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
+  {
+    text: "All of us do not have equal talent. But , all of us have an equal opportunity to develop our talents.",
+    author: "Dr. A.P.J Abdul Kalam",
+  },
 {
 		text: "Winning isnt everything, but wanting it is.",
 		author: "Arnold Palmer"
