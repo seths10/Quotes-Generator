1ea114e055efd78d0ac072650af7feea30b26843
VENNELA2132 <61844572+VENNELA2132@users.noreply.github.com>
2022-10-17 20:10:48 +0530

    Update quotes.js
    Added quote by Rabindranath Tagore

--- js/quotes.js
+++ js/quotes.js
@@ -1918,5 +1918,9 @@ const quotes = [
 	{
 		text: "We accept the love we think we deserve",
 		author: "Todd Anderson"
+	},
+	{
+		text:"Clouds come floating into my life, no longer to carry rain or usher storm, but to add color to my sunset sky.",
+		author: "Rabindranath Tagore"
 	}
 ];
