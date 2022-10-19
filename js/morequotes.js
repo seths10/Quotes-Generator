5764203828e4c0be56fca42e4f5808e1a1f01131
3voltAmperes <3voltAmperes@gmail.com>
2022-10-19 20:48:59 +0200

    Added ten more quotes

---
+++ js/morequotes.js
@@ -0,0 +1,40 @@
+{
+    text: 'Youth has no age',
+    author: null,
+},
+{
+    text: 'Born with nothing, Die with everything',
+    author: 'Papa Roach',
+},
+{
+    text: 'If you can not explain it simply, you do not understand it well enough.',
+    author: 'Albert Einstein',
+},
+{
+    text: 'If you talk enough sense then you will lose your mind',
+    author: 'Amber Run',
+},
+{
+    text: 'We buy things we do not need with money we do not have to impress people we do not like.',
+    author: 'Dave Ramsey',
+},
+{
+    text: 'Reading gives us someplace to go when we have to stay where we are.',
+    author: 'Mason Cooley',
+},
+{
+    text: 'Love is the wish to give, not to receive.',
+    author: 'Bertold Brecht',
+},
+{
+    text: 'Success is stumbling from failure to failure with no loss of enthusiasm.',
+    author: 'Winston Churchill',
+},
+{
+    text: 'You are too concerned with what was and what will be. Yesterday is history, tomorrow is a mystery. But today is a gift- that is why it is called present!',
+    author: 'Kung Fu Panda',
+},
+{
+    text: 'If you fall, I will be there. - ground',
+    author: null,
+},
